fb5d06c26b7cff5691371350ad090438d85dc5ec
GABRIELA VASCONCELOS DOS SANTOS <gabriela.vsantos13@senacsp.edu.br>
2022-04-20 16:38:56 -0300

    trabalhando com Json

---
+++ 5-20042022/js/dados_externos.js
@@ -0,0 +1,10 @@
+// Scripit para dados em formato json e 
+// publicar em um arquivo html
+// vamos usar o comando fetch(buscar) para realizar
+// o carregamento dos dados da url.
+// Com o fetch temos 3 elementos: then, catch e finally, onde
+// then é quando temos um resultado favoravel,catch quando
+// temos algum erro e finally quando precisamos encerrar algo
+// que passou por then ou catch.
+// fetch e uma promisse(promessa) que pode se concretizar com then 
+// ou não com o catch 
